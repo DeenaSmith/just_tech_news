695cfe43cf1a0a87902f68bf0ac3733383d9c3a9
Deena Smith <deenasmith1630@gmail.com>
2022-04-27 16:12:14 -0500

    user routes added and tested, sub folders made

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+
+// Requirements
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+
+// Create connection to database
+const sequelize = new Sequelize('just_tech_news_db', process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+
+
+module.exports = sequelize;
