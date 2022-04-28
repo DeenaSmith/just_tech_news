53b8359e81689bb44b7201aeead4c9908bc144ca
Deena Smith <deenasmith1630@gmail.com>
2022-04-28 12:08:05 -0500

    user authentication and pw comparisons added

--- models/user.js
+++ models/user.js
@@ -1,13 +1,20 @@
 
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
 
 // create our User model
-class User extends Model { }
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
-// define table columns and configuration
+
+
+// create fields/columns for User model
 User.init(
     {
         id: {
@@ -43,7 +50,7 @@ User.init(
                 newUserData.password = await bcrypt.hash(newUserData.password, 10);
                 return newUserData;
             },
-            // set up beforeUpdate lifecycle "hook" functionality
+
             async beforeUpdate(updatedUserData) {
                 updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
                 return updatedUserData;
@@ -57,21 +64,6 @@ User.init(
     }
 );
 
-({
-    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
-    // pass in our imported sequelize connection (the direct connection to our database)
-    sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
-    timestamps: false,
-    // don't pluralize name of database table
-    freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-    underscored: true,
-    // make it so our model name stays lowercase in the database
-    modelName: 'user'
-}
-);
 
 
 
