89fce7d2f92b6bf607910da8e3dc514764606b47
Deena Smith <deenasmith1630@gmail.com>
2022-04-28 14:10:31 -0500

    api endpoints added and tested for posts

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,12 @@
 
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const User = require('./User');
 
 
 // create Post model
 class Post extends Model { }
+
 
 // create fields/columns for Post model
 Post.init(
