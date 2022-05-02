e51a989f8f41669f7996cb9c47cf526457d9236c
Deena Smith <deenasmith1630@gmail.com>
2022-05-02 13:05:46 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,17 @@ require('dotenv').config();
 
 
 // Create connection to database
-const sequelize = new Sequelize('just_tech_news_db', process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 
 
