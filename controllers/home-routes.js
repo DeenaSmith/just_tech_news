295d6dbf9bb2c21ed790df50ff7e75c506c04c6c
Deena Smith <deenasmith1630@gmail.com>
2022-05-04 12:11:05 -0500

    seeds added, created login page and sessions

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,11 @@
 
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Vote } = require('../models');
 
-
+// get all posts for homepage
 router.get('/', (req, res) => {
+    console.log('======================');
     Post.findAll({
         attributes: [
             'id',
@@ -30,6 +31,7 @@ router.get('/', (req, res) => {
     })
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
+
             res.render('homepage', { posts });
         })
         .catch(err => {
@@ -38,5 +40,13 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
 
 module.exports = router;
